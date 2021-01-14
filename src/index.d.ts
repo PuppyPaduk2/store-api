3e81642b1a7e48f0e712be051cbbb168ef8d3244
puppypaduk2 <debraooclark@gmail.com>
2021-01-14 19:43:45 +0300

    feat: export types store, union public

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
+export { StorePublic, StorePublicState, UnionPublic } from "./core/context";
 export { context, getContextState } from "./context";
 export { store } from "./store";
 export { union } from "./union";
