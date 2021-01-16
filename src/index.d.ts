33a801452c359a9ff560ada968cee0d1d47a3abe
puppypaduk2 <debraooclark@gmail.com>
2021-01-16 15:05:20 +0300

    feat: add, export method getRootContextScope

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,6 +4,6 @@ export {
   StorePublicState as StoreState,
   UnionPublic as Union,
 } from "./core/context";
-export { context, getContextState, ContextScope } from "./context";
+export { context, getContextState, getRootContextScope, ContextScope } from "./context";
 export { store, StoreCreator } from "./store";
 export { union, UnionCreator } from "./union";
